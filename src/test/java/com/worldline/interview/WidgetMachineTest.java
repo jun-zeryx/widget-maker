ee01b3c9b954707ecd6adfa8007f495e3f9c2293
Jun Hang <junhang.lum@softspace.com.my>
2021-11-10 20:25:48 +0800

    add initial test class

--- src/test/java/com/worldline/interview/WidgetMachineTest.java
+++ src/test/java/com/worldline/interview/WidgetMachineTest.java
@@ -1,4 +1,12 @@
+package test.java.com.worldline.interview;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class WidgetMachineTest {
-  
+
+    @Test
+    public void produceWidgets() {
+    }
 }
